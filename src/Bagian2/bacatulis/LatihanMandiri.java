7ec30a29c538a70dd3e55a7661c2fa3a469f366b
2410010456pendi-prog <2410010456pendi@gmail.com>
2026-06-23 11:38:28 +0800

    Selesai Bagian 2: Baca Tulis Berkas

---
+++ src/Bagian2/bacatulis/LatihanMandiri.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bagian2.bacatulis;
+
+/**
+ *
+ * @author lenov
+ */
+public class LatihanMandiri {
+    
+}
